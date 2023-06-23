9cb99935e07228c04bf44de1d8ece1afe92db1f8
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2023-06-23 09:14:22 +0200

    Package
    - Documented package module

--- source/jstruct/package.d
+++ source/jstruct/package.d
@@ -1,5 +1,16 @@
+/**
+ * Struct JSON serializer/deserializer
+ */
 module jstruct;
 
+/**
+ * Serialization utilities
+ */
 public import jstruct.serializer;
+
+/**
+ * Deserialization utilities
+ */
 public import jstruct.deserializer;
+
 public import jstruct.exceptions;
